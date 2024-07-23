4c4dd3d5216ebeb37888b359fee64d39a1ae7ae6
Haider Abbas <haidertgr677@gmail.com>
2024-07-23 12:43:24 +0530

    0/1 Knapsack College Program

--- Knapsack.java
+++ Knapsack.java
@@ -1,33 +1,33 @@
 public class Knapsack {
 
     // Method to solve 0/1 Knapsack problem
-    public static int knapSack(int W, int[] weights, int[] profits, int n) {
+    public static int knapSack(int C, int[] capacity, int[] profits, int n) {
         // Create a DP table to store results of subproblems
-        int[][] dp = new int[n + 1][W + 1];
+        int[][] dp = new int[n + 1][C + 1];
 
         // Build the table in a bottom-up manner
         for (int i = 0; i <= n; i++) {
-            for (int w = 0; w <= W; w++) {
-                if (i == 0 || w == 0) {
-                    dp[i][w] = 0;
-                } else if (weights[i - 1] <= w) {
-                    dp[i][w] = Math.max(profits[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            for (int c = 0; c <= C; c++) {
+                if (i == 0 || c == 0) {
+                    dp[i][c] = 0;
+                } else if (capacity[i - 1] <= c) {
+                    dp[i][c] = Math.max(profits[i - 1] + dp[i - 1][c - capacity[i - 1]], dp[i - 1][c]);
                 } else {
-                    dp[i][w] = dp[i - 1][w];
+                    dp[i][c] = dp[i - 1][c];
                 }
             }
         }
 
-        return dp[n][W];
+        return dp[n][C];
     }
 
     public static void main(String[] args) {
-        int[] profits = {60, 100, 120}; // Profits for each item
-        int[] weights = {10, 20, 30}; // Weights for each item
-        int W = 50; // Maximum weight capacity of the knapsack
+        int[] profits = {10, 15, 40}; // Profits for each item
+        int[] capacity = {1, 2, 3}; // Weights for each item
+        int C = 50; // Maximum weight capacity of the knapsack
         int n = profits.length; // Number of items
 
-        int maxProfit = knapSack(W, weights, profits, n);
+        int maxProfit = knapSack(C, capacity, profits, n);
         System.out.println("Maximum profit we can obtain = " + maxProfit);
     }
 }
